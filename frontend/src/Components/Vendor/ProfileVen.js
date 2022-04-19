030d9c270332d14ed21340de5ab0fd19dd45a273
“astha9120” <201801169@daiict.ac.in>
2022-04-19 11:22:46 +0530

    Location_API

--- frontend/src/Components/Vendor/ProfileVen.js
+++ frontend/src/Components/Vendor/ProfileVen.js
@@ -1,22 +1,27 @@
+import React from "react"
 import Header from './Header';
 import TextField from '@mui/material/TextField';
 import { useNavigate } from 'react-router-dom';
 import { useState  , useEffect} from 'react';
 import { makeStyles } from '@mui/styles';
-import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
-import InputAdornment from '@mui/material/InputAdornment';
 import Swal from 'sweetalert2';
-const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import {useRef, useCallback } from 'react'
+import MapGL from 'react-map-gl'
+import Geocoder from 'react-map-gl-geocoder'
+
+
 const axios = require("axios");
 
 
 const useStyles = makeStyles({
     root: {
         minHeight: '100vh',
-        
         fontFamily : "Space Mono, monospace",
         // backgroundColor:'#ddd'
       }, 
@@ -24,8 +29,10 @@ const useStyles = makeStyles({
         paddingTop:"8px"
     },
     Profile:{
-        fontFamily : 'Space Mono',
-        color:"#88888"
+        fontFamily: 'Playfair Display,serif',
+        color:"#e85a4f",
+        paddingTop:"50px",
+        paddingRight:"250px"
     },
    
 
@@ -40,23 +47,51 @@ const ProfileVen = () =>{
 
     const classes = useStyles();
     const navigate = useNavigate();
-    const [state,setState] = useState("")
-    const [city,setCity] = useState("")
+    //const [state,setState] = useState("")
+    //const [city,setCity] = useState("")
     const [charge,setCharge] = useState("")
     const [area,setArea] = useState("")
     const [address,setAddress] = useState("")
     const [phoneno,setPhoneno] = useState("")
     const [name,setName] = useState("")
-    //const [lat,setLat] = useState(0.0)
-    //const [long,setLong] = useState(0.0)
-
-
     const id = localStorage.getItem('id');
+
+    
+
+      const mapBoxToken= process.env.REACT_APP_MAPBOX_TOKEN;
+
+      const [viewport, setViewport] = useState({
+        latitude: 40.7128,
+        longitude: 74.0060,
+        zoom: 8,
+      });
+
+
+      const geocoderContainerRef = useRef();
+      const mapRef = useRef();
+      const handleViewportChange = useCallback(
+        (newViewport) => setViewport(newViewport),
+        []
+      );
+
+    // const set_Add = async(e)=>{
+
+    //     let place=''
+
+    //     place = await e.result.place_name.map(p=>{
+    //         place.append(p)
+    //         if(p==',')
+    //             return place
+    //     })
+
+    //     setArea(place)
+    //     console.log("area is")
+    //     console.log(area)
+
+    // }   
 
     const getData = async () => {
         const response = await axios.get(`http://localhost:4000/vendor/profile/${id}`)
-        setState(response.data[0].state)
-        setCity(response.data[0].city)
         setCharge(response.data[0].charge)
         setArea(response.data[0].area)
         setAddress(response.data[0].address)
@@ -72,37 +107,14 @@ const ProfileVen = () =>{
     const submit = async(e)=>{
         e.preventDefault();
 
-        const mapBoxToken= process.env.REACT_APP_MAPBOX_TOKEN;
-        
-        const geocoder=mbxGeocoding({
-            accessToken:mapBoxToken,
-            types: 'country,region,place,postcode,locality,neighborhood,address'
-        });
-    
-
-        const add = `${address} , ${area} , ${city} , ${state} , United States`
-
-        const GeoData=await geocoder.forwardGeocode({
-            query:add,
-            limit:1
-        }).send()
-        
-
-        // setLat(GeoData.body.features[0].center[1])
-        // setLong(GeoData.body.features[0].center[0])
-        console.log("location cor")
-        console.log(GeoData.body.features[0])
-
         const result = await axios.put(`http://localhost:4000/vendor/profile/${id}`,{
             phoneno:phoneno,
             address:address,
             area:area,
             charge:charge,
             name:name,
-            latitude:GeoData.body.features[0].center[1],
-            longitude:GeoData.body.features[0].center[0],
-            city:city,
-            state:state
+            latitude:viewport.latitude,
+            longitude:viewport.longitude,
         })
 
         console.log(result.data)
@@ -131,7 +143,8 @@ const ProfileVen = () =>{
 
 
     return(
-       <div><Header />
+       <div>
+        <Header />
         <Grid container component="main"   className={classes.root}>
              
             <Grid item lg={6} md={4} xs={2}>
@@ -139,17 +152,14 @@ const ProfileVen = () =>{
                     justifyContent="space-evenly"
                     alignItems="center" 
                     spacing={5}>
-                    <Grid item lg={2} marginLeft="auto">
-                         
-                    </Grid>
-                    <Grid item lg={2} marginLeft="85%">
-                        <Typography component="h4" variant="h4" className={classes.Profile}>Profile</Typography>
+                    <Grid item lg={2}>
+                        <Typography component="h4" variant="h4" className={classes.Profile}>PROFILE</Typography>
                     </Grid>
                     <Grid item lg={7} className={classes.form}>
-                    <TextField
+                            <TextField
                                 required
                                 sx={{ width: '40ch',
-                                marginLeft:"75%",
+                                marginLeft:"10%",
                                 marginBottom:"20px"}}
                                 id="name"
                                 label="Name"
@@ -162,7 +172,7 @@ const ProfileVen = () =>{
                             <TextField
                                 required
                                 sx={{ width: '40ch',
-                                marginLeft:"75%",
+                                marginLeft:"10%",
                                 marginBottom:"20px"}}
                                 id="phoneno"
                                 label="Phone Number"
@@ -176,10 +186,10 @@ const ProfileVen = () =>{
                             <TextField
                                 required
                                 sx={{ width: '40ch',
-                                marginLeft:"75%",
+                                marginLeft:"10%",
                                 marginBottom:"20px"}}
                                 id="address"
-                                label="Address"
+                                label="House number"
                                 name="address"
                                 value={address!=null ? address : ""}
                                 onChange={(e)=>setAddress(e.target.value)}
@@ -190,58 +200,10 @@ const ProfileVen = () =>{
                             <TextField
                                 required
                                 sx={{ width: '40ch',
-                                marginLeft:"75%",
-                                marginBottom:"20px"}}
-                                id="area"
-                                label="Area"
-                                name="area"
-                                value={area!=null ? area : ""}
-                                onChange={(e)=>setArea(e.target.value)}
-                                autoComplete="area"
-                                autoFocus
-                            />  
-
-                        <TextField
-                                required
-                                sx={{ width: '40ch',
-                                marginLeft:"75%",
-                                marginBottom:"20px"}}
-                                id="city"
-                                label="City"
-                                name="city"
-                                value={city}
-                                onChange={(e)=>setCity(e.target.value)}
-                                autoComplete="city"
-                                autoFocus
-                                // disabled={true}
-                            />
-                            
-                        <TextField
-                                required
-                                sx={{ width: '40ch',
-                                marginLeft:"75%",
-                                marginBottom:"20px"}}
-                                id="state"
-                                label="State"
-                                name="state"
-                                value={state}
-                                onChange={(e)=>setState(e.target.value)}
-                                autoComplete="state"
-                                autoFocus
-                                // disabled={true}
-                            />
-
-                            
-                          
-                            
-                    
-                            <TextField
-                                required
-                                sx={{ width: '40ch',
-                                marginLeft:"75%",
+                                marginLeft:"10%",
                                 marginBottom:"20px"}}
                                 id="charge"
-                                label="Charge"
+                                label="Charge per month"
                                 name="charge"
                                 value={charge!=null ? charge : ""}
                                 onChange={(e)=>setCharge(e.target.value)}
@@ -254,7 +216,7 @@ const ProfileVen = () =>{
                             <Button
                                 type="submit"
                                 margin="normal"
-                                sx={{ width: '44ch',marginLeft:"75%",marginTop:"20px",marginBottom:"30px"}}
+                                sx={{ width: '44ch',marginLeft:"10%",marginTop:"20px",marginBottom:"30px"}}
                                 variant="contained"
                                 onClick={submit}>
                                 {charge===null ? "Submit" : "Update"}
@@ -265,8 +227,28 @@ const ProfileVen = () =>{
                     
                 </Grid>
             </Grid>
+            <Grid item lg={6} md={4} xs={2}>
+                <div style={{marginTop:"50px"}} ref={geocoderContainerRef}/>
+                    <MapGL
+                        ref={mapRef}
+                        {...viewport}
+                        width="90%"
+                        height="500px"
+                        onViewportChange={handleViewportChange}
+                        mapboxApiAccessToken={mapBoxToken}
+                    >
+                        <Geocoder
+                            mapRef={mapRef}
+                            containerRef={geocoderContainerRef}
+                            onViewportChange={handleViewportChange}
+                            mapboxApiAccessToken={mapBoxToken}
+                            position="top-left"
+                            onResult = {(r)=>setArea(r.result.place_name)}
+                        />
+                    </MapGL>
+            </Grid>
         </Grid>
-        </div>
+    </div>
     )
 
 }
