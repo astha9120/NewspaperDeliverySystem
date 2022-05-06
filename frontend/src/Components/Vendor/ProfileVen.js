8b52093899784a6dd9aa5684eeb5280edde06475
Maulina22 <maulin2210@gmail.com>
2022-05-06 10:58:10 +0530

    profile_map

--- frontend/src/Components/Vendor/ProfileVen.js
+++ frontend/src/Components/Vendor/ProfileVen.js
@@ -1,105 +1,139 @@
-import React from "react"
 import Header from './Header';
 import TextField from '@mui/material/TextField';
 import { useNavigate } from 'react-router-dom';
 import { useState  , useEffect} from 'react';
 import { makeStyles } from '@mui/styles';
+import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
+import InputAdornment from '@mui/material/InputAdornment';
 import Swal from 'sweetalert2';
 import { styled } from '@mui/material/styles';
 
-import 'mapbox-gl/dist/mapbox-gl.css'
+
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import {useRef, useCallback } from 'react'
-import MapGL from 'react-map-gl'
-import Geocoder from 'react-map-gl-geocoder'
-
+// import MapGL from 'react-map-gl'
+// import Geocoder from 'react-map-gl-geocoder'
+import mapboxgl from '!mapbox-gl';// eslint-disable-line import/no-webpack-loader-syntax
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 const axios = require("axios");
 
-
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
 const useStyles = makeStyles({
-    vendor_root: {
+    cust_root: {
+        // backgroundColor:'#ddd'
         minHeight: '100vh',
         fontFamily:'Playfair Display,serif',
-        // backgroundColor:'#ddd'
       }
   });
 
   const StyledButton = styled(Button)({
     '&:hover': {
-        backgroundColor: '#A7423A',
+        backgroundColor: '#e85a4f',
         boxShadow: '20',
         width: '48ch'
       }
   })
 
 
-
 const ProfileVen = () =>{
 
     
-
+   
     const classes = useStyles();
     const navigate = useNavigate();
-    const [charge,setCharge] = useState("")
     const [area,setArea] = useState("")
     const [address,setAddress] = useState("")
     const [phoneno,setPhoneno] = useState("")
     const [name,setName] = useState("")
     const [profile_stat,setProfile_stat] = useState("Your profile has not been verified")
+    const [charge,setCharge] = useState("")
+
     const id = localStorage.getItem('id');
+    
+    const mapBoxToken= process.env.REACT_APP_MAPBOX_TOKEN;
+    
+    const mapContainer = useRef(null);
+    const navigationControl = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-73.935242);
+    const [lat, setLat] = useState(40.730610);
+    const [zoom, setZoom] = useState(11.03);
 
     
 
-      const mapBoxToken= process.env.REACT_APP_MAPBOX_TOKEN;
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+          container: mapContainer.current,
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [lng, lat],
+          zoom: zoom
+        });
+        navigationControl.current = new mapboxgl.NavigationControl();
+        map.current.addControl(navigationControl.current);
+      }, []);
+    
+      useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+          setLng(map.current.getCenter().lng.toFixed(4));
+          setLat(map.current.getCenter().lat.toFixed(4));
+          setZoom(map.current.getZoom().toFixed(2));
 
-      const [viewport, setViewport] = useState({
-        latitude: 40.7128,
-        longitude: -74.0060,
-        zoom: 8,
-      });
+        });
+        map.current.addControl(
+            new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl
+            })
+            );
+          
+           
+      },[]);
+    
+      
 
-
-      const geocoderContainerRef = useRef();
-      const mapRef = useRef();
-      const handleViewportChange = useCallback(
-        (newViewport) => setViewport(newViewport),
-        []
-      );
 
     const getData = async () => {
-        const response = await axios.get(`${process.env.REACT_APP_URL}/vendor/profile/${id}`)
+        const response = await axios.get(`${process.env.REACT_APP_URL}/vendor/profile/${id}`);
         setCharge(response.data[0].charge)
         setArea(response.data[0].area)
         setAddress(response.data[0].address)
         setPhoneno(response.data[0].phoneno)
         setName(response.data[0].name)
+        setLat(response.data[0].latitude)
+        setLng(response.data[0].longitude)
         if(response.data[0].accept==1)
-            setProfile_stat("Your profile is verified")
-        
+        setProfile_stat("Your profile is verified")
     }
     
     useEffect(() => {
         getData();
     }, []);
 
+  
+  
     const submit = async(e)=>{
-        e.preventDefault();
-        console.log(area)
 
+        e.preventDefault();
+         
+        
+        
         const result = await axios.put(`${process.env.REACT_APP_URL}/vendor/profile/${id}`,{
             phoneno:phoneno,
             address:address,
-            area:area,
-            charge:charge,
+            area:map.current._controls[3].lastSelected != null ? JSON.parse(map.current._controls[3].lastSelected).place_name:area,
             name:name,
-            latitude:viewport.latitude,
-            longitude:viewport.longitude,
+            latitude:lat,
+            longitude:lng,
+            charge:charge,
         })
-
         console.log(result.data)
         if(result.data==="yes"){
             Swal.fire({
@@ -119,31 +153,28 @@ const ProfileVen = () =>{
                 showConfirmButton: false,
                 timer:   1500
           })
-          
         }
-        //window.location.reload(true)
     }
 
 
     return(
-       <div style={{backgroundColor:"#eae7dc"}}>
-        <Header />
-        <Grid container component="main" className={classes.vendor_root}>
-             
+        <div style={{backgroundColor:"#eae7dc"}}>
+            <Header />
+        <Grid container component="main"   className={classes.cust_root}>
+            
             <Grid item lg={6} md={4} xs={2}>
                 <Grid container direction="column"  
                     justifyContent="space-evenly"
                     alignItems="center" 
                     spacing={5}>
                     <Grid item lg={2}>
-                         
                         <Typography variant="h4"
                             style={{ fontFamily: 'Playfair Display,serif',color:"#e85a4f",paddingTop:"50px",paddingRight:"150px"}}
                         > PROFILE
                         </Typography>
                     </Grid>
-                    <Grid item lg={7}>
-                            <TextField
+                    <Grid item lg={7} className={classes.form}>
+                    <TextField
                                 required
                                 sx={{ width: '40ch',
                                 marginLeft:"15%",
@@ -176,15 +207,15 @@ const ProfileVen = () =>{
                                 marginLeft:"15%",
                                 marginBottom:"20px"}}
                                 id="address"
-                                label="House number"
+                                label="House no"
                                 name="address"
                                 value={address!=null ? address : ""}
                                 onChange={(e)=>setAddress(e.target.value)}
                                 autoComplete="address"
                                 autoFocus
                             />
-
-                            <TextField
+            
+            <TextField
                                 required
                                 sx={{ width: '40ch',
                                 marginLeft:"15%",
@@ -201,38 +232,19 @@ const ProfileVen = () =>{
                             <StyledButton
                                 type="submit"
                                 margin="normal"
-                                sx={{ width: '48ch',marginLeft:"15%",marginTop:"20px",marginBottom:"30px",backgroundColor:"#e85a4f",
-                            }}
+                                sx={{ width: '48ch',marginLeft:"15%",marginTop:"20px",marginBottom:"30px"
+                                ,backgroundColor:"#e85a4f"}}
                                 variant="contained"
                                 onClick={submit}>
                                 {charge===null ? "Submit" : "Update"}
                             </StyledButton>
                     </Grid>
-    
-                
-                    
-                </Grid>
+               </Grid>
             </Grid>
             <Grid item lg={6} md={4} xs={2}>
-                <div style={{marginTop:"50px"}} ref={geocoderContainerRef}/>
-                    <MapGL
-                        ref={mapRef}
-                        {...viewport}
-                        width="90%"
-                        height="500px"
-                        onViewportChange={handleViewportChange}
-                        mapboxApiAccessToken={mapBoxToken}
-                    >
-                        <Geocoder
-                            required
-                            mapRef={mapRef}
-                            containerRef={geocoderContainerRef}
-                            onViewportChange={handleViewportChange}
-                            mapboxApiAccessToken={mapBoxToken}
-                            position="top-left"
-                            onResult = {(r)=>setArea(r.result.place_name)}
-                        />
-                    </MapGL>
+                {/* <div style={{marginTop:"50px"}} ref={geocoderContainerRef}/> */}
+                <div ref={mapContainer} style={{height:"100%"}} onResult = {(r)=>setArea(r.result.place_name) }/>
+                    
             </Grid>
             <Typography align="center" marginTop="40px"
                 marginBottom="30px" height="35px" width="100%" fontSize="20px" 
@@ -240,8 +252,7 @@ const ProfileVen = () =>{
                 {profile_stat}
             </Typography>
         </Grid>
-        
-    </div>
+        </div>
     )
 
 }
--- frontend/src/Components/ndb/ProfileNdb.js
+++ frontend/src/Components/ndb/ProfileNdb.js
@@ -3,101 +3,137 @@ import TextField from '@mui/material/TextField';
 import { useNavigate } from 'react-router-dom';
 import { useState  , useEffect} from 'react';
 import { makeStyles } from '@mui/styles';
+import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
+import InputAdornment from '@mui/material/InputAdornment';
 import Swal from 'sweetalert2';
+import { styled } from '@mui/material/styles';
 
-import 'mapbox-gl/dist/mapbox-gl.css'
+
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import {useRef, useCallback } from 'react'
-import MapGL from 'react-map-gl'
-import Geocoder from 'react-map-gl-geocoder'
-import { styled } from '@mui/material/styles';
-import mapboxgl from 'mapbox-gl';// eslint-disable-line import/no-webpack-loader-syntax
-
+// import MapGL from 'react-map-gl'
+// import Geocoder from 'react-map-gl-geocoder'
+import mapboxgl from '!mapbox-gl';// eslint-disable-line import/no-webpack-loader-syntax
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 const axios = require("axios");
 
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
 const useStyles = makeStyles({
-    ndb_root: {
+    cust_root: {
+        // backgroundColor:'#ddd'
         minHeight: '100vh',
         fontFamily:'Playfair Display,serif',
-      }, 
+      }
   });
 
   const StyledButton = styled(Button)({
     '&:hover': {
-        backgroundColor: '#A7423A',
+        backgroundColor: '#e85a4f',
         boxShadow: '20',
         width: '48ch'
       }
   })
 
 
-
 const ProfileNdb = () =>{
 
     
-
+   
     const classes = useStyles();
     const navigate = useNavigate();
-    const [charge,setCharge] = useState("")
     const [area,setArea] = useState("")
     const [address,setAddress] = useState("")
     const [phoneno,setPhoneno] = useState("")
     const [name,setName] = useState("")
     const [profile_stat,setProfile_stat] = useState("Your profile has not been verified")
-
+    const [charge,setCharge] = useState("")
 
     const id = localStorage.getItem('id');
-    console.log("id "+id)
-
+    
     const mapBoxToken= process.env.REACT_APP_MAPBOX_TOKEN;
+    
+    const mapContainer = useRef(null);
+    const navigationControl = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-73.935242);
+    const [lat, setLat] = useState(40.730610);
+    const [zoom, setZoom] = useState(11.03);
 
-      const [viewport, setViewport] = useState({
-        latitude: 40.7128,
-        longitude: -74.0060,
-        zoom: 8,
-      });
+    
 
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+          container: mapContainer.current,
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [lng, lat],
+          zoom: zoom
+        });
+        navigationControl.current = new mapboxgl.NavigationControl();
+        map.current.addControl(navigationControl.current);
+      }, []);
+    
+      useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+          setLng(map.current.getCenter().lng.toFixed(4));
+          setLat(map.current.getCenter().lat.toFixed(4));
+          setZoom(map.current.getZoom().toFixed(2));
 
-      const geocoderContainerRef = useRef();
-      const mapRef = useRef();
-      const handleViewportChange = useCallback(
-        (newViewport) => setViewport(newViewport),
-        []
-      );
+        });
+        map.current.addControl(
+            new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl
+            })
+            );
+          
+           
+      },[]);
+    
+      
+
 
     const getData = async () => {
-        const response = await axios.get(`${process.env.REACT_APP_URL}/ndb/profile/${id}`)
+        const response = await axios.get(`${process.env.REACT_APP_URL}/ndb/profile/${id}`);
         setCharge(response.data[0].charge)
         setArea(response.data[0].area)
         setAddress(response.data[0].address)
         setPhoneno(response.data[0].phoneno)
         setName(response.data[0].name)
+        setLat(response.data[0].latitude)
+        setLng(response.data[0].longitude)
         if(response.data[0].accept==1)
-            setProfile_stat("Your profile is verified")
-        
+        setProfile_stat("Your profile is verified")
     }
     
     useEffect(() => {
         getData();
     }, []);
 
+  
+  
     const submit = async(e)=>{
-        e.preventDefault();
 
+        e.preventDefault();
+         
+        
+        
         const result = await axios.put(`${process.env.REACT_APP_URL}/ndb/profile/${id}`,{
             phoneno:phoneno,
             address:address,
-            area:area,
-            charge:charge,
+            area:map.current._controls[3].lastSelected != null ? JSON.parse(map.current._controls[3].lastSelected).place_name:area,
             name:name,
-            latitude:viewport.latitude,
-            longitude:viewport.longitude,
+            latitude:lat,
+            longitude:lng,
+            charge:charge,
         })
-
         console.log(result.data)
         if(result.data==="yes"){
             Swal.fire({
@@ -125,7 +161,7 @@ const ProfileNdb = () =>{
     return(
         <div style={{backgroundColor:"#eae7dc"}}>
             <Header />
-        <Grid container component="main"   className={classes.ndb_root}>
+        <Grid container component="main"   className={classes.cust_root}>
             
             <Grid item lg={6} md={4} xs={2}>
                 <Grid container direction="column"  
@@ -133,13 +169,12 @@ const ProfileNdb = () =>{
                     alignItems="center" 
                     spacing={5}>
                     <Grid item lg={2}>
-                    <Typography variant="h4"
+                        <Typography variant="h4"
                             style={{ fontFamily: 'Playfair Display,serif',color:"#e85a4f",paddingTop:"50px",paddingRight:"150px"}}
                         > PROFILE
                         </Typography>
                     </Grid>
-                    <Grid item lg={7}>
-                    
+                    <Grid item lg={7} className={classes.form}>
                     <TextField
                                 required
                                 sx={{ width: '40ch',
@@ -180,8 +215,8 @@ const ProfileNdb = () =>{
                                 autoComplete="address"
                                 autoFocus
                             />
-                    
-                            <TextField
+            
+            <TextField
                                 required
                                 sx={{ width: '40ch',
                                 marginLeft:"15%",
@@ -195,8 +230,6 @@ const ProfileNdb = () =>{
                                 autoFocus
                             />
 
-                            
-
                             <StyledButton
                                 type="submit"
                                 margin="normal"
@@ -204,32 +237,15 @@ const ProfileNdb = () =>{
                                 ,backgroundColor:"#e85a4f"}}
                                 variant="contained"
                                 onClick={submit}>
-                                {charge===null ? "Submit" : "Update"}
+                                {area===null ? "Submit" : "Update"}
                             </StyledButton>
-                    </Grid>  
-            
-                </Grid>
+                    </Grid>
+               </Grid>
             </Grid>
             <Grid item lg={6} md={4} xs={2}>
-                <div style={{marginTop:"50px"}} ref={geocoderContainerRef}/>
-                    <MapGL
-                        ref={mapRef}
-                        {...viewport}
-                        width="90%"
-                        height="500px"
-                        onViewportChange={handleViewportChange}
-                        mapboxApiAccessToken={mapBoxToken}
-                    >
-                        <Geocoder
-                            required
-                            mapRef={mapRef}
-                            containerRef={geocoderContainerRef}
-                            onViewportChange={handleViewportChange}
-                            mapboxApiAccessToken={mapBoxToken}
-                            position="top-left"
-                            onResult = {(r)=>setArea(r.result.place_name)}
-                        />
-                    </MapGL>
+                {/* <div style={{marginTop:"50px"}} ref={geocoderContainerRef}/> */}
+                <div ref={mapContainer} style={{height:"100%"}} onResult = {(r)=>setArea(r.result.place_name) }/>
+                    
             </Grid>
             <Typography align="center" marginTop="40px"
                 marginBottom="30px" height="35px" width="100%" fontSize="20px" 
